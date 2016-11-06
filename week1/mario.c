3482f88f3a722758ec3e410212e0c2f93f4b347b
LeoKunHao <leo-huang@live.com>
2016-11-06 10:55:11 -0800

    Create mario.c

---
+++ week1/mario.c
@@ -0,0 +1,47 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+    
+    char end[24] = {""};
+    printf("height: ");
+
+    int n = GetInt();
+    
+    while (n > 23) {
+        printf("Retry: ");
+        n = GetInt();
+    }
+
+    if (n == 1){
+        printf("##\n");
+    }
+    if (n < 0 ) {
+        printf("\n"); 
+    }
+
+    int counter = 2;
+    int nn = n+1;
+    
+    if (n>1 && n<24) {
+        
+        for(int j = 0; j < n; j++){
+            
+            for(int a = 0; a < nn-counter; a++){
+                strcat(end, " ");
+            }
+            for(int b = 0; b < counter; b++){
+                strcat(end, "#");
+            }
+            
+            printf("%s\n", end);
+            counter++;
+            
+            strcpy(end,"");
+            
+        }
+        
+    }
+
+}
